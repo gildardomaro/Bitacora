cedd6fd3cfe25f541a03e5ac648a6b4200e9ae7c
Gildardo Maro Valenzuela <gildardo.maro@imss.gob.mx>
2026-09-22 17:44:28 -0700

    Auto-update: datos de trading 2026-09-22 17:44

--- static_data.js
+++ static_data.js
@@ -1,5 +1,5 @@
 window.STATIC_DASHBOARD_DATA = {
-  generated_at: "2026-09-23T00:14:27.657365Z",
+  generated_at: "2026-09-23T00:44:27.654475Z",
   "account_real1000": {
     "terminal": "Vantage REAL (REAL1000)",
     "connected": true,
@@ -26522,5 +26522,5 @@ window.STATIC_DASHBOARD_DATA = {
       }
     }
   },
-  "last_sync": "2026-09-22 17:14:27"
+  "last_sync": "2026-09-22 17:44:27"
 };
