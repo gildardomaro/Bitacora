dc6602b2fb0ba440145292a8799fd5e5f28c36af
Gildardo Maro Valenzuela <gildardo.maro@imss.gob.mx>
2026-09-23 18:14:25 -0700

    Auto-update: datos de trading 2026-09-23 18:14

--- static_data.js
+++ static_data.js
@@ -1,5 +1,5 @@
 window.STATIC_DASHBOARD_DATA = {
-  generated_at: "2026-09-24T00:44:27.139618Z",
+  generated_at: "2026-09-24T01:14:25.161865Z",
   "account_real1000": {
     "terminal": "Vantage REAL (REAL1000)",
     "connected": true,
@@ -38,10 +38,10 @@ window.STATIC_DASHBOARD_DATA = {
     "login": 61587621,
     "server": "Pepperstone-Demo",
     "balance": 1130.47,
-    "equity": 1030.01,
+    "equity": 1036.83,
     "margin": 73.25,
-    "margin_free": 956.76,
-    "floating": -99.33,
+    "margin_free": 963.58,
+    "floating": -92.51,
     "positions_count": 1,
     "dynamic_lot": 0.03,
     "next_lot_balance": 1200.0,
@@ -52,10 +52,10 @@ window.STATIC_DASHBOARD_DATA = {
         "type": "BUY",
         "volume": 0.11,
         "open_price": 1.33186,
-        "current_price": 1.32283,
+        "current_price": 1.32345,
         "sl": 1.31588,
         "tp": 1.33346,
-        "profit": -99.33,
+        "profit": -92.51,
         "time": "2026-09-22 23:45:00"
       }
     ],
@@ -27063,5 +27063,5 @@ window.STATIC_DASHBOARD_DATA = {
       }
     }
   },
-  "last_sync": "2026-09-23 17:44:27"
+  "last_sync": "2026-09-23 18:14:25"
 };
