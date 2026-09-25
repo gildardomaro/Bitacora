7abecc135e14765042bcbb44fded04176b9ac91f
Gildardo Maro Valenzuela <gildardo.maro@imss.gob.mx>
2026-09-25 06:44:27 -0700

    Auto-update: datos de trading 2026-09-25 06:44

--- dashboard/static_data.js
+++ dashboard/static_data.js
@@ -1,18 +1,18 @@
 window.STATIC_DASHBOARD_DATA = {
-  generated_at: "2026-09-25T13:14:26.842387Z",
+  generated_at: "2026-09-25T13:44:26.752302Z",
   "account_real1000": {
     "terminal": "Vantage REAL (REAL1000)",
     "connected": true,
     "login": 27497962,
     "server": "VantageMarkets-Live 5",
-    "balance": 2362.25,
-    "equity": 2862.25,
+    "balance": 0.0,
+    "equity": 0.0,
     "margin": 0.0,
-    "margin_free": 2862.25,
+    "margin_free": 0.0,
     "floating": 0.0,
     "positions_count": 0,
-    "dynamic_lot": 0.05,
-    "next_lot_balance": 2400.0,
+    "dynamic_lot": 0.02,
+    "next_lot_balance": 2100.0,
     "positions": [],
     "strategy": "MARO REAL1000"
   },
@@ -38,10 +38,10 @@ window.STATIC_DASHBOARD_DATA = {
     "login": 61587621,
     "server": "Pepperstone-Demo",
     "balance": 1130.47,
-    "equity": 1062.53,
+    "equity": 1058.24,
     "margin": 73.25,
-    "margin_free": 989.28,
-    "floating": -66.44,
+    "margin_free": 984.99,
+    "floating": -70.73,
     "positions_count": 1,
     "dynamic_lot": 0.03,
     "next_lot_balance": 1200.0,
@@ -52,10 +52,10 @@ window.STATIC_DASHBOARD_DATA = {
         "type": "BUY",
         "volume": 0.11,
         "open_price": 1.33186,
-        "current_price": 1.32582,
+        "current_price": 1.3254299999999999,
         "sl": 1.31588,
         "tp": 1.33346,
-        "profit": -66.44,
+        "profit": -70.73,
         "time": "2026-09-22 23:45:00"
       }
     ],
@@ -28185,5 +28185,5 @@ window.STATIC_DASHBOARD_DATA = {
       }
     }
   },
-  "last_sync": "2026-09-25 06:14:26"
+  "last_sync": "2026-09-25 06:44:26"
 };
